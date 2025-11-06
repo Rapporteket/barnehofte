ea3dcc3d51721bf88a7f8c35e6034686c92239f1
Arnfinn Hykkerud Steindal <arnfinn.hykkerud.steindal@helse-nord.no>
2025-11-06 09:55:40 +0100

    Endret fra rapRegTemplate til barnehofte

--- R/ui.R
+++ R/ui.R
@@ -1,11 +1,11 @@
-#' Client (ui) for the rapRegTemplate app
+#' Client (ui) for the barnehofte app
 #'
 #' @return An shiny app ui object
 #' @export
 
 app_ui <- function() {
 
-  regTitle <- "rapRegTemplate"
+  regTitle <- "barnehofte"
 
   shiny::tagList(
     shiny::navbarPage(
